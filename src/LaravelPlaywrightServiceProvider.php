cd1ff7f95e5752dcf2a423a1c6741b19a9620a7a
Lee Overy <me@leeovery.com>
2023-01-16 16:53:15 +0000

    wip

--- src/LaravelPlaywrightServiceProvider.php
+++ src/LaravelPlaywrightServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace Leeovery\LaravelPlaywright;
 
+use Spatie\LaravelPackageTools\Package;
 use Leeovery\LaravelPlaywright\Commands\LaravelPlaywrightEnvSetup;
 use Leeovery\LaravelPlaywright\Commands\LaravelPlaywrightEnvTeardown;
-use Spatie\LaravelPackageTools\Package;
+use Leeovery\LaravelPlaywright\Commands\Database\DropDatabaseCommand;
+use Leeovery\LaravelPlaywright\Commands\Database\CreateDatabaseCommand;
 use Spatie\LaravelPackageTools\PackageServiceProvider as ServiceProvider;
+use Leeovery\LaravelPlaywright\Commands\Database\ReCreateDatabaseCommand;
 
 class LaravelPlaywrightServiceProvider extends ServiceProvider
 {
@@ -18,6 +21,9 @@ class LaravelPlaywrightServiceProvider extends ServiceProvider
             ->hasCommands(
                 LaravelPlaywrightEnvSetup::class,
                 LaravelPlaywrightEnvTeardown::class,
+                CreateDatabaseCommand::class,
+                DropDatabaseCommand::class,
+                ReCreateDatabaseCommand::class,
             );
     }
 }
